914ddbcded8cbba46a35f5d049f443dfbc2c28fb
GuanhuaWang <alexwgh333@gmail.com>
2016-12-04 19:30:44 -0800

    w/o checkpoint running time measurements

--- wordcount/src/main/scala/wordapp.scala
+++ wordcount/src/main/scala/wordapp.scala
@@ -7,19 +7,27 @@ import org.apache.spark.rdd.RDD
 
 object wordcount {
 	def main(args: Array[String]){
-		val logFile = "file:///Users/guanhua/Desktop/spark-hadoop2.6/README.md"
+        val timestamp1: Long = System.currentTimeMillis
+		val logFile = "file:///Users/guanhua/Desktop/5G.txt"
 		val conf = new SparkConf().setAppName("Word Count")
     	val sc = new SparkContext(conf)
-    	val logData = sc.textFile(logFile, 2)
+    	val logData = sc.textFile(logFile, 5)
     	sc.setCheckpointDir("data/checkpoint")
     	val map_counts = logData.flatMap(line => line.split(" ")).map(word => (word, 1))
-    	map_counts.checkpoint
+        val timestamp4: Long = System.currentTimeMillis
+    	map_counts.checkpoint()
+        val timestamp2: Long = System.currentTimeMillis
+        val checkpoint_time = timestamp2 - timestamp1
+        println("===========================checkpoint happens "+checkpoint_time+"ms===========================")
     	//map_counts.collect().foreach(println) 
         //val result : RDD[Any]= sc.checkpointFile("data/checkpoint/07ebd902-7bf9-4df3-9c35-c5c43cd4c73c/rdd-3")
     	//result.asInstanceOf[T]
         //val rdd : RDD[(String, Int)]= RDDUtilsInSpark.getCheckpointRDD(sc, "data/checkpoint/07ebd902-7bf9-4df3-9c35-c5c43cd4c73c/rdd-3")
         val reduce_counts = map_counts.reduceByKey(_ + _)
     	reduce_counts.saveAsTextFile("file:///Users/guanhua/Desktop/spark-hadoop2.6/wordcounts")
+        val timestamp3: Long = System.currentTimeMillis
+        val total_Time = timestamp3 - timestamp1
+        println("===========================The total time is " + total_Time +"ms===========================")
         //reduce_counts.collect().foreach(println)        
 	}
 }
